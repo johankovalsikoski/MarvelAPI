afa0d4ac384a00f07e68d2c4d26f299d84133f61
Johan Kovalsikoski <johank@dbserver.com.br>
2018-05-16 16:06:01 -0300

    Solving warnings

--- app/src/main/java/com/kovalsikoski/johan/marvelapi/RetrofitInitializer.kt
+++ app/src/main/java/com/kovalsikoski/johan/marvelapi/RetrofitInitializer.kt
@@ -5,12 +5,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInitializer {
 
-    private val BASE_URL : String = "http://gateway.marvel.com/"
-
     private val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl("http://gateway.marvel.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-    fun marvelService() = retrofit.create(MarvelService::class.java)
+    fun marvelService() = retrofit.create(MarvelService::class.java)!!
 }
