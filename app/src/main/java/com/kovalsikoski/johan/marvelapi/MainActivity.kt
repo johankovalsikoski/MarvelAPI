e00826bd033b176fda92efffa559c01565245c84
Johan Kovalsikoski <johank@dbserver.com.br>
2018-05-16 16:35:29 -0300

    Implementation of recyclerview initializer, alertdialog builder and some refacts. - (MISS YOU SMALL COMMITS)

--- app/src/main/java/com/kovalsikoski/johan/marvelapi/MainActivity.kt
+++ app/src/main/java/com/kovalsikoski/johan/marvelapi/MainActivity.kt
@@ -1,104 +1,95 @@
 package com.kovalsikoski.johan.marvelapi
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.util.*
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CharacterAdapter
+    private lateinit var progressDialog: AlertDialog
+    private lateinit var charactersList: MutableList<MarvelModel.MarvelPage.Characters>
+
+    private val ts = Date().time.toString()
+    private val hash = createHash(ts)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        test()
+        buildAlertDialogForProgress()
+
+        firstLoad(ts, hash)
 
     }
 
-    private fun test(){
-        val call = RetrofitInitializer().marvelService().getCharactersPage("v1/public/characters?ts=1526419169643&apikey=63c845f07f40335b0a40a3942bcc5ca8&hash=9ad4455630555fbb82024ab5d605dc55")
-//        val call = RetrofitInitializer().marvelService().getCharactersPage("v1/public/characters?ts=1526419169643&apikey=63c845f07f40335b0a40a3942bcc5ca8&hash=9ad4455630555fbb82024ab5d605dc55")
+    private fun firstLoad(ts: String, hash: String){
+        progressDialog.show()
 
-        /* /v1/public/characters?offset=50&apikey */
+        val call = RetrofitInitializer().marvelService().getCharactersFirstPage(ts, getString(R.string.public_key),hash)
+
         call.enqueue(object : Callback<MarvelModel> {
             override fun onResponse(call: Call<MarvelModel>?, response: Response<MarvelModel>?) {
                 if (response?.body()?.code == 200) {
                     response.body()!!.data.results.let {
-                        it.forEach {
-                            Log.d("eoq", it.name)
-                            println("=========================>${it.name}")
-                        }
+                        charactersList.addAll(it)
+                        progressDialog.dismiss()
+                        initRecyclerView()
                     }
                 } else {
-                    //dialog erro conexão
+                    progressDialog.dismiss()
+                    Toast.makeText(this@MainActivity, getString(R.string.cant_load_content), Toast.LENGTH_LONG).show()
                 }
             }
 
             override fun onFailure(call: Call<MarvelModel>?, t: Throwable?) {
-                Log.e("onFailure", t?.message)
+                Log.e("error", t?.message)
+                progressDialog.dismiss()
             }
         })
     }
 
-    private fun md5(): String {
+    private fun createHash(ts: String): String {
 
-        val ts = Date().time
         val hash = "$ts${getString(R.string.private_key)}${getString(R.string.public_key)}"
 
         val messageDigest = MessageDigest.getInstance("MD5")
         messageDigest.update(hash.toByteArray(), 0, hash.length)
 
         return BigInteger(1, messageDigest.digest()).toString(16)
-
     }
 
-    /*
-    private fun getAllCharacters() {
+    private fun initRecyclerView(){
+        recyclerView = findViewById(R.id.character_recyclerview)
+        adapter = CharacterAdapter(charactersList, this)
+        recyclerView.adapter = adapter
 
-        progressDialog.show()
-
-        if (firstRun) {
-            call = RetrofitInitializer().starWarsService().getCharacters()
-        } else if (!firstRun) {
-            call = RetrofitInitializer().starWarsService().getCharactersPage(nextPageUrl!!)
-        }
-
-        call.enqueue(object : Callback<CharactersPage> {
-            override fun onFailure(call: Call<CharactersPage>?, t: Throwable?) {
-                println(t?.message)
-            }
-
-            override fun onResponse(call: Call<CharactersPage>?, response: Response<CharactersPage>?) {
-                response?.body()?.let {
-
-                    if (it.next != null) {
-                        hasNext = true
-                        nextPageUrl = it.next
-                    } else {
-                        hasNext = false
-                    }
-
-                    it.results.forEach {
-                        charactersList.add(it)
-                    }
-                }
-                firstRun = false
-
-                if(hasNext) {
-                    getAllCharacters()
-                } else {
-                    initRecyclerView()
-                    progressDialog.dismiss()
-                    recyclerView.visibility = View.VISIBLE
-                }
-            }
-        })
+        val layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = layoutManager
     }
-    */
+
+    private fun buildAlertDialogForProgress() {
+        val dialogView = View.inflate(this, R.layout.custom_layout_progressbar, null)
+        val builder = AlertDialog.Builder(this)
+        val message = dialogView.findViewById<TextView>(R.id.messageDialog)
+
+        message.text = getString(R.string.loading_progressbar)
+        builder.setView(dialogView)
+        builder.setCancelable(false)
+        progressDialog = builder.create()
+    }
 }
