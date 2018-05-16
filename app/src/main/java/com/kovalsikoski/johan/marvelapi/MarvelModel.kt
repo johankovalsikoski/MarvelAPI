38a98353c47d05695170578168c52d096758fb85
Johan Kovalsikoski <johank@dbserver.com.br>
2018-05-16 15:32:59 -0300

    Param added to marvel page

--- app/src/main/java/com/kovalsikoski/johan/marvelapi/MarvelModel.kt
+++ app/src/main/java/com/kovalsikoski/johan/marvelapi/MarvelModel.kt
@@ -1,8 +1,8 @@
 package com.kovalsikoski.johan.marvelapi
 
-data class MarvelModel (var code: Int, var status: String, var data: MarvelPage) {
+data class MarvelModel (var code: Int, var data: MarvelPage) {
 
-    inner class MarvelPage (var total: Int, var count: Int, var results: List<Characters>) {
+    inner class MarvelPage (var offset: Int, var total: Int, var count: Int, var results: List<Characters>) {
 
         inner class Characters (var name: String)
     }
